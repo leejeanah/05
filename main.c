35658408aea637b5565d1ff51af56b3cb172e6b9
leejeanah <ijina590@naver.com>
2022-10-04 17:06:51 +0900

    03

--- main.c
+++ main.c
@@ -5,14 +5,20 @@
 
 int main(int argc, char *argv[]) {
 	
-	int x;
-	printf("정수 하나를 입력하시오: ");
-	scanf("%i", &x);
+	char c;
+	int num = 0;
+	printf("input a string: ");
 	
-	if(x < 0)
-	 printf("절대값은 %i입니다.\n", -x);
-	else
-	 printf("절대값은 %i입니다.\n", x);
+	while((c = getchar()) != '\n') //입력문자가 나올때까지 반복 
+	{
+		if( c >= '0' && c <= '9')
+		 num = num + 1;
+		else
+		 continue;	
+	}
+	
+	
+	printf("the number of digits is %i", num);
 	 
 	return 0;
 }
