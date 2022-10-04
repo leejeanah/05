3b523ad91c0d06b5bd5e059fd4aa1566eb4f76d5
leejeanah <ijina590@naver.com>
2022-10-04 17:21:35 +0900

    04

--- main.c
+++ main.c
@@ -5,20 +5,17 @@
 
 int main(int argc, char *argv[]) {
 	
-	char c;
-	int num = 0;
-	printf("input a string: ");
+	int sum = 0;
+	int x;
+	int i;
 	
-	while((c = getchar()) != '\n') //입력문자가 나올때까지 반복 
-	{
-		if( c >= '0' && c <= '9')
-		 num = num + 1;
-		else
-		 continue;	
-	}
+	printf("input a number: ");
+	scanf("%d", &x);
 	
-	
-	printf("the number of digits is %i", num);
+	for(i = 0; i <= x; i++)
+	 sum = sum + i;
+	 
+	printf("the result is %i.\n", sum);
 	 
 	return 0;
 }
